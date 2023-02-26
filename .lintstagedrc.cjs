04aa11461f1070493f42c10f3f1b3d4b31e945c2
halvin <halvin@nexttrucking.com>
2023-02-26 21:47:59 +0800

    test

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  "*.{md,css}": "prettier --list-different --ignore-unknown --write",
-  "**/*.{ts?(x)}": ["pnpm tsc", "pnpm lint"],
-  "**/*.{js?(x)}": ["pnpm lint"],
+  // "*.{md,css}": "prettier --list-different --ignore-unknown --write",
+  "**/*.{tsx,ts}": ["pnpm tsc"],
+  "**/*.{tsx,ts,jsx,js}": ["pnpm lint"],
 }
